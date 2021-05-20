313e2c595257e879f12ec7fc57579da4fd41542b
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-05-21 11:45:33 +1200

    size alert updated

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -2,6 +2,7 @@ import "../styles/Item.scss";
 import Button from "../components/Button";
 import { useContext, useState } from "react";
 import { MyCartContext } from "../contexts/MyCartContext";
+import { FaLongArrowAltLeft } from "react-icons/fa";
 
 const Item = ({ data }) => {
     const { addItem } = useContext(MyCartContext);
@@ -9,6 +10,7 @@ const Item = ({ data }) => {
     const [mediumClicked, setMediumClicked] = useState(null);
     const [largeClicked, setLargeClicked] = useState(null);
     const [sizeSelected, setSizeSelected] = useState('');
+    const [sizeAlert, setSizeAlert] = useState(false);
 
     const handleSizeBtn = (e) => {
         switch (e.target.innerText.toLowerCase()) {
@@ -39,7 +41,11 @@ const Item = ({ data }) => {
     };
     const handleAddBtn = () => {
         if (!sizeSelected) {
-            alert("Please select size!");
+            // alert("Please select size!");
+            setSizeAlert(true);
+            setTimeout(() => {
+                setSizeAlert(false);
+            }, 2000);
         } else {
             addItem(data.title, data.price, sizeSelected, data.img, data.imgAlt);
             setSizeSelected('');
@@ -67,6 +73,7 @@ const Item = ({ data }) => {
                         <button className={smallClicked ? "size-btn s--active" : "size-btn"} onClick={handleSizeBtn}>S</button>
                         <button className={mediumClicked ? "size-btn m--active" : "size-btn"} onClick={handleSizeBtn}>M</button>
                         <button className={largeClicked ? "size-btn l--active" : "size-btn"} onClick={handleSizeBtn}>L</button>
+                        <p className={sizeAlert ? "sizeAlert active" : "sizeAlert"}><FaLongArrowAltLeft className="arrowL" />Choose your size!</p>
                     </div>
                     <Button btnStyle="btn--outline" btnSize="btn--medium" className="btn-addToCart" onClick={handleAddBtn}>ADD TO CART</Button>
                 </div>
