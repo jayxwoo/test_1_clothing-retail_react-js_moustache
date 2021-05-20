d5a0141898016fb7f286a074e2336c52a1d305c8
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-05-21 07:05:09 +1200

    commented code removed

--- src/data/stockData.js
+++ src/data/stockData.js
@@ -8,30 +8,4 @@ export const item_01 = {
     img: img_01,
     imgAlt: 'Classic Tee',
     id: uuid(),
-}
-
-// export const item_01 = {
-//     title: 'Classic Tee',
-//     price: 75,
-//     desc: 'Dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Quod autem ratione actum est, id officium appellamus dolor sit amet, consectetur adipiscing elit. Haec et tu ita posuisti, et verba vestra sunt. Quod autem ratione actum est, id officium appellamus',
-//     sizes: [
-//         {
-//             size: 'S',
-//             stockNum: 10,
-//             id: uuid(),
-//         },
-//         {
-//             size: 'M',
-//             stockNum: 10,
-//             id: uuid(),
-//         },
-//         {
-//             size: 'L',
-//             stockNum: 10,
-//             id: uuid(),
-//         }
-//     ],
-//     img: img_01,
-//     imgAlt: 'Classic Tee',
-//     id: uuid(),
-// }
+};
